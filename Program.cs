cc82154c8ad2b1e60a8377c2662fb852568f8366
Pavani <pavanigottipati539@gmail.com>
2021-11-12 15:19:40 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// struct demo
+namespace Demo
+{
+    interface IMath
+    {
+        int add(int x, int y);
+        int sub(int x, int y);
+    }
+
+    class Math:IMath
+    {
+        public int add(int x, int y)
+        { return x + y; }
+
+        public int sub(int x, int y)
+        { return x - y; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            IMath math = new Math();
+            Console.WriteLine(math.add(10, 20));
+        }
+    }
+}
